7fae37e5b61ca7398210f6ad1a1f582e286880e6
yang <yang@getblueshift.com>
2023-02-27 11:27:57 -0800

    upgrade xandra that fixes a bug

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule EctoXandra.MixProject do
       {:ecto, "~> 3.9"},
       {:ecto_sql, "~> 3.9"},
       {:nimble_lz4, "~> 0.1.2"},
-      {:xandra, git: "https://github.com/blueshift-labs/xandra.git", tag: "v0.14.0-bsft-17"},
+      {:xandra, git: "https://github.com/blueshift-labs/xandra.git", tag: "v0.14.0-bsft-18"},
       {:jason, "~> 1.2"}
     ]
   end
