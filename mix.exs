7c65a33fd122dbe25979a3b024500da82db7e7b0
Sagar Rohankar <sagar.rohankar@blueshift.com>
2025-09-08 13:11:00 +0530

    update xandra dep

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule EctoXandra.MixProject do
       {:ecto, "~> 3.11"},
       {:ecto_sql, "~> 3.11"},
       {:nimble_lz4, "~> 0.1.2", optional: true},
-      {:xandra, git: "https://github.com/blueshift-labs/xandra.git", tag: "v0.14.0-bsft-27"},
+      {:xandra,
+       git: "https://github.com/blueshift-labs/xandra.git", branch: "nil-rpc_address-fix"},
       {:jason, "~> 1.2"}
     ]
   end
